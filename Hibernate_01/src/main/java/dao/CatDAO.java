a17762f02919e9de2cc69ddae4465cc75db1e197
Trung_Hieu_Dev <dangtrunghieusg@gmail.com>
2023-01-28 21:38:51 +0700

    hql crud

---
+++ Hibernate_01/src/main/java/dao/CatDAO.java
@@ -0,0 +1,138 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import model.Cat;
+import util.HibernateUtil;
+
+public class CatDAO implements DAOInterface<Cat> {
+
+	@Override
+	public List<Cat> selectAll() {
+		List<Cat> list = new ArrayList();
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if(sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				
+				Transaction tr = session.beginTransaction();
+				
+				String hql = "from Cat";
+				Query query = session.createQuery(hql);
+				list = query.getResultList();
+				
+				tr.commit();
+				
+				session.close();
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	@Override
+	public Cat selectById(Cat t) {
+		List<Cat> list = new ArrayList();
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if(sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				
+				Transaction tr = session.beginTransaction();
+				
+				String hql = "from Cat c WHERE c.id=:id";
+				Query query = session.createQuery(hql);
+				query.setParameter("id", t.getId());
+				list = query.getResultList();
+				
+				tr.commit();
+				
+				session.close();
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if (list.size() > 0) {
+			return list.get(0);
+		} else {
+			return null;
+		}
+	}
+
+
+	@Override
+	public boolean insert(Cat t) {
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if(sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				
+				Transaction tr = session.beginTransaction();
+				
+				session.save(t);
+				
+				tr.commit();
+				
+				session.close();
+				return true;
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean update(Cat t) {
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if(sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				
+				Transaction tr = session.beginTransaction();
+				
+				session.saveOrUpdate(t);
+				
+				tr.commit();
+				
+				session.close();
+				return true;
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	@Override
+	public boolean delete(Cat t) {
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if(sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				
+				Transaction tr = session.beginTransaction();
+				
+				session.delete(t);
+				
+				tr.commit();
+				
+				session.close();
+				return true;
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
--- Hibernate_01/src/main/java/test/TestCat.java
+++ Hibernate_01/src/main/java/test/TestCat.java
@@ -2,15 +2,20 @@ package test;
 
 
 
+import java.sql.Date;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import dao.CatDAO;
 import model.Cat;
 import util.HibernateUtil;
 
 public class TestCat {
 	public static void main(String[] args) {
+		/*
 		try {
 			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 			if (sessionFactory != null) {
@@ -27,8 +32,37 @@ public class TestCat {
 				}
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			
 		}
+		*/
+		CatDAO cat_Dao = new CatDAO();
+		
+		/*
+		 *Get list all
+		List<Cat> list = cat_Dao.selectAll();
+		for (Cat cat : list) {
+			System.out.println(cat);
+		}
+		*/
+		
+		
+		//get by id
+		/* Cat c = new Cat();
+		c.setId(1);
+		
+		Cat c1 = cat_Dao.selectById(c);
+		System.out.println(c1);
+		*/
+		
+		
+		//insert
+//		Cat c3 = new Cat("Meo mum", new Date(System.currentTimeMillis()), true);
+//		cat_Dao.insert(c3);
+		
+		//delete
+		Cat c4 = new Cat();
+		c4.setId(3);
+		cat_Dao.delete(c4);
 		
 	}
 }
